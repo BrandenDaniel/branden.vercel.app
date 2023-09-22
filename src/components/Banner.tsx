69c4658ba14ba44afa1a96cf768cab8c06346b95
Branden Ng <brandendanielng@gmail.com>
2023-09-23 02:05:44 +1000

    additional personal info

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import Image from "next/image";
 import profilePic from "../assets/images/profile.png";
 import Socials from "./Socials";
 import Link from "next/link";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { useState } from "react";
 
 const Banner = () => {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <header className="banner">
       <div className="banner__bio">
         <h1>{`hey, I'm branden 👋`}</h1>
-        <p>{`I'm a frontend developer, personal finance nerd, and minimalist.`}</p>
+        <p>
+          {`I'm a frontend developer, personal finance nerd, and minimalist.`}{" "}
+          {!showMore && (
+            <button onClick={() => setShowMore(true)}>
+              <FiMoreHorizontal />
+            </button>
+          )}
+        </p>
+
+        {showMore && (
+          <>
+            <p>
+              {"I enjoy turning complex challenges into a intuitive products."}
+            </p>
+            <p>{"My current focus is React (Next.js) and TypeScript."}</p>
+          </>
+        )}
+
         <div className="banner__links">
           <Socials />
           <Link href="/docs/CV.pdf" target="_blank" download>

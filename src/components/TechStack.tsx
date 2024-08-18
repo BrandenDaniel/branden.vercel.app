eebe0359e6f167b5f27b006578ad7febaa71d11b
Branden Ng <brandendanielng@gmail.com>
2024-08-18 14:20:49 +1000

    Tech stack updates

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,40 +1,96 @@
+import {FaHtml5} from 'react-icons/fa'
+import {FaUniversalAccess} from 'react-icons/fa6'
+import {FaCss3} from 'react-icons/fa'
+import {SiTailwindcss} from 'react-icons/si'
+import {IoLogoJavascript} from 'react-icons/io5'
+import {FaReact} from 'react-icons/fa'
+import {SiNextdotjs} from 'react-icons/si'
+import {SiPreact} from 'react-icons/si'
+import {FaSass} from 'react-icons/fa'
+
 const TechStack = () => {
-  return (
-    <section className="techStack">
-      <div>
-        <p>
-          HTML <span>4 years</span>
-        </p>
-        <p>
-          Accessibility
-          <span>4 years</span>
-        </p>
-      </div>
+	return (
+		<section className='techStack'>
+			<div>
+				<p>
+					<a
+						href='https://html.com/'
+						target='_blank'>
+						<FaHtml5 /> HTML
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://www.w3.org/WAI/fundamentals/accessibility-intro/'
+						target='_blank'>
+						<FaUniversalAccess /> Accessibility
+					</a>
+				</p>
+			</div>
 
-      <div>
-        <p>
-          CSS <span>4 years</span>
-        </p>
-        <p>
-          Sass <span>4 years</span>
-        </p>
-      </div>
-      <div>
-        <p>
-          JavaScript <span>4 years</span>
-        </p>
-        <p>
-          React.js <span>2 year</span>
-        </p>
-        <p>
-          Next.js <span>{`< 1 year`}</span>
-        </p>
-        <p>
-          TypeScript <span>{`< 1 year`}</span>
-        </p>
-      </div>
-    </section>
-  );
-};
+			<div>
+				<p>
+					<a
+						href='https://www.w3.org/TR/CSS/#css'
+						target='_blank'>
+						<FaCss3 />
+						CSS
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://sass-lang.com/'
+						target='_blank'>
+						<FaSass />
+						Sass
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://tailwindcss.com/'
+						target='_blank'>
+						<SiTailwindcss />
+						Tailwindcss
+					</a>
+				</p>
+			</div>
+			<div>
+				<p>
+					<a
+						href='https://www.javascript.com/'
+						target='_blank'>
+						<IoLogoJavascript />
+						JavaScript
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://react.dev/'
+						target='_blank'>
+						{' '}
+						<FaReact />
+						React.js
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://nextjs.org/'
+						target='_blank'>
+						<SiNextdotjs />
+						Next.js
+					</a>
+				</p>
+				<p>
+					<a
+						href='https://preactjs.com/'
+						target='_blank'>
+						<SiPreact />
+						Preact.js
+					</a>
+				</p>
+			</div>
+		</section>
+	)
+}
 
-export default TechStack;
+export default TechStack
